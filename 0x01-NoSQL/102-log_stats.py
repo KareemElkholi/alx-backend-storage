2659740149b3aec7d61d0ff729d94c35a7aa2e75
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-06-22 04:20:17 +0300

    Correction

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     print(f"{logs.count_documents({'path': '/status'})} status check\nIPs:")
     ips = logs.aggregate([
             {"$group": {"_id": "$ip", "count": {"$count": {}}}},
-            {"$sort": {"count": -1}}, {"$limit": 10}])
+            {"$sort": {"count": -1}}, {"$limit": 10},
+            {"$project": {"count": 1}}])
     for ip in ips:
-        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+        print(f"\t{ip['_id']}: {ip['count']}")
