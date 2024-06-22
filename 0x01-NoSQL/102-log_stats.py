e144a7a3a4e409abaf758259fb00290b51dc587f
KareemElkholi <kareemshrf@std.mans.edu.eg>
2024-06-22 04:12:23 +0300

    Correction

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """15. Log stats - new version"""
-from pymongo import MongoClient
-logs = MongoClient().logs.nginx
-print(f"{logs.count_documents({})} logs\nMethods:")
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    print(f"    method {method}: {logs.count_documents({'method': method})}")
-print(f"{logs.count_documents({'path': '/status'})} status check\nIPs:")
-ips = logs.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$count": {}}}},
-        {"$sort": {"count": -1}}, {"$limit": 10}])
-for ip in ips:
-    print(f"    {ip['_id']}: {ip['count']}")
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    logs = MongoClient().logs.nginx
+    print(f"{logs.count_documents({})} logs\nMethods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(f"\tmethod {method}: {logs.count_documents({'method': method})}")
+    print(f"{logs.count_documents({'path': '/status'})} status check\nIPs:")
+    ips = logs.aggregate([
+            {"$group": {"_id": "$ip", "count": {"$count": {}}}},
+            {"$sort": {"count": -1}}, {"$limit": 10}])
+    for ip in ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
